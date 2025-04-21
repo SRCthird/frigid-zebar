0cd8d5f1e630de5b7ca32ded0a7a703fbb476312
Stephen Chryn <M312559@one.merckgroup.com>
2025-04-21 14:02:27 -0400

    Convert base zebar view into vite project

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './css/styles.css'
+import '../../normalize.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
